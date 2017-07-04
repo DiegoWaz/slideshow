766fa1054086a41ffafc991da1f673d0aeb8c841
singh <mohit.hgto@gmail.com>
2017-07-04 11:08:28 +0200

    add autoreplay

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,46 +1,76 @@
 $(document).ready(function () {
 
+
+
+
 		var slideIndex = 1;
-		showSlides(slideIndex);
+		showSlidesArrow(slideIndex);
 
-		$('#next').click(plusSlides);
-		$('#before').click(plusSlides);
+        play();
 
-		
+        function play() {
+            var slides = $(".mySlides");
+            if(slides.length <= slideIndex) {
+                    thatInterval = setInterval(function(){
+                    showSlidesArrow(slideIndex ++);
+                }, 5000);
+            }
+        }
 
-		function plusSlides(n) {
-			 showSlides(slideIndex += n);
-		}
+        function pause(){
+            clearInterval(thatInterval);
+        }
 
-		function currentSlide(n) {
-		  showSlides(slideIndex = n);
-		}
+        // Aller au slide suivant
+        $(".next").click(function(){
+            plusSlides(1);
+        });
 
-		function showSlides(n) {
-		  var i;
-		  var slides = document.getElementsByClassName("mySlides");
-		  var dots = document.getElementsByClassName("dot");
-		  
+        // Aller au slide précédent
+        $(".before").click(function(){
+            plusSlides(-1);
+        });
 
-		  if (n > slides.length) {
-		  	slideIndex = 1
-		  }
+        // Mettre en pause lorsqu'on passe la souris par dessus le slideshow
+        $(".slideshow-container").mouseover(function(){
+            clearInterval(thatInterval);
+        });
 
-		  if (n < 1) {
-		  	slideIndex = slides.length
-		  }
+        // Direction slide
+        function plusSlides(n) {
+             showSlidesArrow(slideIndex += n);
+        }
 
-		  for (i = 0; i < slides.length; i++) {
-		      slides[i].style.display = "none";  
-		  }
+        // Slide actuel
+        function currentSlide(n) {
+          showSlidesArrow(slideIndex = n);
+        }
 
-		  for (i = 0; i < dots.length; i++) {
-		      dots[i].className = dots[i].className.replace(" active", "");
-		  }
-		  
-		  slides[slideIndex-1].style.display = "block";  
-		  dots[slideIndex-1].className += " active";
-		}
+        function showSlidesArrow(n) {
+
+            var i;
+            var slides = $(".mySlides");
+            var dots = $(".dot");
+
+            if (n > slides.length) {
+                slideIndex = 1
+            }
+
+            if (n < 1) {
+                slideIndex = slides.length
+            }
+
+            for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";  
+            }
+
+            for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" active", "");
+            }
+
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+        }
 
 		$.ajax({
 			type: "POST",
@@ -55,12 +85,28 @@ $(document).ready(function () {
 				var descimg  = ListSlide[slide].desc;
 				var id  = ListSlide.length;
 				var height = 600;
+				var width = 100;
 				
- 	      	//$(".slideshow-container").append('<div class="imgslide"><img src="'+ srcimg + '"><div class="text"><span class="title">'+ titleimg +'</span><span class="description">'+ descimg +'</span></div></div>');
+ 	      	/*//$(".slideshow-container").append('<div class="imgslide"><img src="'+ srcimg + '"><div class="text"><span class="title">'+ titleimg +'</span><span class="description">'+ descimg +'</span></div></div>');
  	      	$(".slideshow-container").prepend('<div class="mySlides fade"><img src="'+ srcimg + '"style="width:100%"><div id="container"><h1>'+ titleimg +'</h1><h2>'+ descimg +'</h2></div></div>');
 // 	      	//$("#slideshow .tabs").append('<div class="tab-slide"><span class="title">'+ titleimg +'</span><span class="description">'+ descimg +'</span></div>');
 	      	$(".mySlides img").css({"height":height+"px"});
-// 	      	$("#slideshow .slide img").css({"height":height+"px"});
+// 	      	$("#slideshow .slide img").css({"height":height+"px"});*/
+			
+
+			// Mettre en pause lorsqu'on passe la souris par dessus le slideshow
+        $(".slideshow-container").mouseover(function(){
+            clearInterval(thatInterval);
+        });
+
+		$(".slideshow-container").prepend('<div class="mySlides fade"><img src="'+ srcimg + '"><div id="container"><h1>'+ titleimg +'</h1><h2>'+ descimg +'</h2></div></div>');
+                  //$("#slideshow .tabs").append('<div class="tab-slide"><span class="title">'+ titleimg +'</span><span class="description">'+ descimg +'</span></div>');
+                  $(".mySlides img").css({
+                      "height": height+"px",
+                      "width": width+"%",
+                  });
+
+
  	   }
 
  	   	for (i = 0; i < ListSlide.length; i++) {
@@ -70,7 +116,11 @@ $(document).ready(function () {
 	   	
 		}
 
-    $(document).ready(function(){
+    
+
+	 })
+
+$(document).ready(function(){
         $("#container h1").animate({letterSpacing: "+=10px"},1500);
         $("#container h1").animate({letterSpacing: "-=10px"},1500);
         }),
@@ -79,12 +129,9 @@ $(document).ready(function () {
         $("#container h1").animate({letterSpacing: "+=10px"},1500);
         $("#container h1").animate({letterSpacing: "-=10px"},1500);
         }),
-        $(".prev").click(function(){
+        $(".before").click(function(){
         $("#container h1").animate({letterSpacing: "+=10px"},1500);
         $("#container h1").animate({letterSpacing: "-=10px"},1500);
     	});
     });
-
-	 })
-
 
